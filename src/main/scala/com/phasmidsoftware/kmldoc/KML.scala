fead01dd0b213fdb5ce6d0727b15d5b964cd4778
Robin Hillyard <r.hillyard@northeastern.edu>
2022-07-26 16:55:10 -0400

    Successfully reads KML file and shows a couple of attributes

--- src/main/scala/com/phasmidsoftware/kmldoc/KML.scala
+++ src/main/scala/com/phasmidsoftware/kmldoc/KML.scala
@@ -1,17 +1,29 @@
 package com.phasmidsoftware.kmldoc
 
+import com.phasmidsoftware.kmldoc.XmlUtils.maybeString
+
 import java.net.URL
 import scala.xml.{Elem, Node, NodeSeq, XML}
 
-case class KML()
+case class KML(xmlns: Option[String], documents: Seq[Document])
 
-case class Document(name: String, description: String)
+case class Document(name: String, description: Option[String])
 
 object Document {
-  def fromXML(node: Node): NodeSeq = node \ "Document"
+  def fromXML(node: Node): Document = {
+    val name: NodeSeq = node \ "name"
+    val description: NodeSeq = node \ "description"
+    Document(maybeString(name).getOrElse("unnamed"), maybeString(description))
+  }
 }
 
 object KML {
+  def fromXML(node: Node): KML = {
+    val xmlns: NodeSeq = node \ "@xmlns"
+    val docs: NodeSeq = node \ "Document"
+    KML(maybeString(xmlns), docs map (n => Document.fromXML(n)))
+  }
+
   def loadKML(resource: URL): KML = {
     require(resource != null)
     loadKML(resource.getPath)
@@ -20,15 +32,16 @@ object KML {
   def loadKML(file: String): KML = {
     require(file != null)
     val xml: Elem = XML.loadFile(file)
-    val kmls: collection.Seq[NodeSeq] = for (kml <- xml \\ "kml") yield Document.fromXML(kml)
-    println(kmls.size)
-    val kml = kmls.headOption
-    println(kml)
-    KML()
+    val kmls: collection.Seq[KML] = for (kml <- xml \\ "kml") yield KML.fromXML(kml)
+    kmls.head
   }
+}
 
+object XmlUtils {
+  def maybeString(ns: NodeSeq): Option[String] = ns.headOption.map(n => n.text)
 }
 
 object Test extends App {
-  KML.loadKML(KML.getClass.getResource("sample.kml"))
+  val kml: KML = KML.loadKML(KML.getClass.getResource("sample.kml"))
+  println(s"KML: $kml")
 }
