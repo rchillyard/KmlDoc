6b7c4182dcb92e3e214c77da12cdf01553aaa667
Robin Hillyard <r.hillyard@northeastern.edu>
2024-12-24 12:53:53 -0500

    Added some unit tests to ExtractorSpec

--- src/test/scala/com/phasmidsoftware/xml/ExtractorSpec.scala
+++ src/test/scala/com/phasmidsoftware/xml/ExtractorSpec.scala
@@ -1,76 +1,122 @@
 package com.phasmidsoftware.xml
 
+import com.phasmidsoftware.core.FP
+import org.scalatest.Assertion
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import scala.util.{Failure, Try}
+import scala.xml.Node
 
 class ExtractorSpec extends AnyFlatSpec with should.Matchers {
 
-    behavior of "Extractor"
+  behavior of "Extractor"
 
-    it should "apply" in {
+  it should "extract" in {
+    val target: Extractor[Int] = (node: Node) => FP.optionToTry(node.text.toIntOption, new NoSuchElementException())
+    target.extract(<junk>1</junk>) shouldBe Try(1)
+    target.extract(<junk>one</junk>) should matchPattern { case Failure(_) => }
+  }
 
-    }
+  it should "map" in {
+    val node = <junk>1</junk>
+    val ext: Extractor[Int] = (node: Node) => FP.optionToTry(node.text.toIntOption, new NoSuchElementException())
+    val target: Extractor[Int] = ext.map(x => x + 1)
+    target.extract(node) shouldBe Try(2)
+  }
 
-    it should "booleanExtractor" in {
+  it should "flatMap" in {
+    val node = <junk>1</junk>
+    val ext: Extractor[Int] = (node: Node) => FP.optionToTry(node.text.toIntOption, new NoSuchElementException())
+    val target: Extractor[Int] = ext.flatMap(x => Try(x + 1))
+    target.extract(node) shouldBe Try(2)
+  }
 
-    }
+  it should "$bar" in {
 
-    it should "doubleExtractor" in {
+  }
 
-    }
+  it should "optionalAttribute" in {
 
-    it should "extract" in {
+  }
 
-    }
+  it should "plural" in {
 
-    it should "extractAll" in {
+  }
 
-    }
+  it should "logger" in {
 
-    it should "extractChildrenDeprecated" in {
+  }
 
-    }
+  it should "createLazy" in {
 
-    it should "extractElementsByLabel" in {
+  }
 
-    }
+  it should "booleanExtractor" in {
 
-    it should "fieldExtractor" in {
+  }
 
-    }
+  it should "doubleExtractor" in {
 
-    it should "extractMulti" in {
+  }
 
-    }
+  it should "extractElementsByLabel" in {
 
-    it should "intExtractor" in {
+  }
 
-    }
+  it should "inferAttributeType" in {
 
-    it should "longExtractor" in {
+  }
 
-    }
+  it should "attribute" in {
 
-    it should "map" in {
+  }
 
-    }
+  it should "apply1" in {
 
-    it should "optionalAttribute" in {
+  }
 
-    }
+  it should "apply2" in {
 
-    it should "plural" in {
+  }
 
-    }
+  it should "optional" in {
 
-    it should "$bar" in {
+  }
 
-    }
+  it should "extractAll" in {
 
-    behavior of "TagToExtractorFunc"
+  }
 
-    it should "apply" in {
+  it should "unitExtractor" in {
 
-    }
+  }
+
+  it should "extractSingleton" in {
+
+  }
+
+  it should "extractMulti" in {
+
+  }
+
+  it should "longExtractor" in {
+
+  }
+
+  it should "fieldExtractor" in {
+
+  }
+
+  it should "extractChildren" in {
+
+  }
+
+  it should "intExtractor" in {
+
+  }
+
+  it should "extractSequence" in {
+
+  }
 
 }
